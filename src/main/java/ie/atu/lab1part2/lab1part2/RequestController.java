0825440f18da6bb2a1e83934f4d8f71f752d75f9
G00391846@atu.ie <g00391846@atu.ie>
2023-09-27 10:23:52 +0100

    Added get mapping method

--- src/main/java/ie/atu/lab1part2/lab1part2/RequestController.java
+++ src/main/java/ie/atu/lab1part2/lab1part2/RequestController.java
@@ -1,4 +1,13 @@
 package ie.atu.lab1part2.lab1part2;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RequestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello ! How is your day going ?";
+    }
 }
