011973be148781f1e8de4966411b1f7b1b9deea6
G00391846@atu.ie <g00391846@atu.ie>
2023-09-27 10:39:28 +0100

    Added pathVariable

--- src/main/java/ie/atu/lab1part2/lab1part2/RequestController.java
+++ src/main/java/ie/atu/lab1part2/lab1part2/RequestController.java
@@ -1,6 +1,7 @@
 package ie.atu.lab1part2.lab1part2;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +11,7 @@ public class RequestController {
     public String hello() {
         return "Hello ! How is your day going ?";
     }
+
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable String name) {return "Name: " + name;}
 }
