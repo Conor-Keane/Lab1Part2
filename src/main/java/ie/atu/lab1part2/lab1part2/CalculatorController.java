4ed36f0a88a06d502ff9bd5b06edb4850494d24c
G00391846@atu.ie <g00391846@atu.ie>
2023-10-02 15:32:10 +0100

    Added Calculate method

--- src/main/java/ie/atu/lab1part2/lab1part2/CalculatorController.java
+++ src/main/java/ie/atu/lab1part2/lab1part2/CalculatorController.java
@@ -1,4 +1,13 @@
 package ie.atu.lab1part2.lab1part2;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CalculatorController {
+    @GetMapping("/calculate")
+    public String calculate(@RequestParam int num1, @RequestParam int num2, @RequestParam String operation) {
+        return "Num1 = " + num1 + "Num2 = " + num2;
+    }
 }
