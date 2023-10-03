25a43343287cb5097d3653dc04208085d335bf50
conorkeane <g00391846@atu.ie>
2023-10-03 21:22:52 +0100

    Added variables to CalculatorClass

--- src/main/java/ie/atu/lab1part2/lab1part2/CalculatorClass.java
+++ src/main/java/ie/atu/lab1part2/lab1part2/CalculatorClass.java
@@ -1,10 +1,31 @@
 package ie.atu.lab1part2.lab1part2;
 
 public class CalculatorClass {
-    private int num1;
-    private int num2;
-    private int ans;
+    private int val1;
+    private int val2;
+    private int result;
 
+    public int getVal1() {
+        return val1;
+    }
 
+    public void setVal1(int val1) {
+        this.val1 = val1;
+    }
 
+    public int getVal2() {
+        return val2;
+    }
+
+    public void setVal2(int val2) {
+        this.val2 = val2;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
 }
